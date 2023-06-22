9ad6734b848d922e7463daece27d59fa81bd1714
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-06-21 20:26:00 -0400

    pushing to update env vars on netlify

--- Relink/src/components/headers/Header.jsx
+++ Relink/src/components/headers/Header.jsx
@@ -20,6 +20,7 @@ export default function Header() {
   useEffect(() => {
     console.log(import.meta.env.VITE_SERVER_DOMAIN);
     console.log(import.meta.env.VITE_LOCAL_DOMAIN);
+    console.log(import.meta.env.VITE_IPINFO_KEY);
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
