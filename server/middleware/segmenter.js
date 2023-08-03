1c3ab1722a31fc776ae9764d6061f5ed159d36bd
JaredCaprio <88853874+JaredCaprio@users.noreply.github.com>
2023-08-03 18:54:01 -0400

    make segmenter middleware function use async await

--- server/middleware/segmenter.js
+++ server/middleware/segmenter.js
@@ -2,19 +2,19 @@ const axios = require("axios");
 
 module.exports = {
   segmentText: async (req, res, next) => {
-    axios
-      .post(`${process.env.FLASK_URL}/segmentText`, {
+    try {
+      const res = await axios.post(`${process.env.FLASK_URL}/segmentText`, {
         body: req.body.body,
-      })
-      .then((data) => {
-        console.log(req.body.body, "input");
-        console.log(data, "output");
-        req.body.body = data.data;
-        console.log(req.body.body, "REQUEST.BODY.BODY!!!AFTER AXIOS");
-      })
-      .catch((error) => {
-        console.log(error);
       });
+
+      console.log(req.body.body, "input");
+      console.log(res.data, "output");
+
+      req.body.body = res.data;
+    } catch (error) {
+      console.log(error);
+      return next(error);
+    }
 
     return next();
   },
